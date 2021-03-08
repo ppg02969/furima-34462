3a055aa2fd649ffcb02dc85dc9fc478bc665b183
ppg02969 <bibliomania76ex@gmail.com>
2021-03-08 14:25:39 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_fee_id, :area_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
