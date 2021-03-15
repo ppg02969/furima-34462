8eebdd595bfe942a4ec79911b51d000b5d18bf54
ppg02969 <bibliomania76ex@gmail.com>
2021-03-15 12:44:28 +0900

    商品購入機能 修正4

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -39,7 +39,7 @@ private
 
   def move_to_index
     @item = Item.find(params[:item_id])
-    if @item.user_id == current_user.id
+    if @item.user_id == current_user.id || @item.order.present?
       redirect_to root_path
     end
   end
